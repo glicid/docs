331d286ffcb18c53ad7bf0a485d9a4c55cfc975a
Mir Junaid <mir.jnd@gmail.com>
2023-09-19 00:50:39 +0200

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/tutorial-basics/intro.md">
             Getting Started with GLiCID ⏱️
           </Link>
         </div>
