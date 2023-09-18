fe2206f87a909f6a158fb1674742fc49121e6cc6
Mir Junaid <mir.jnd@gmail.com>
2023-09-19 00:59:39 +0200

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="~/git_repo/docs/intro.md">
+            to="/docs/intro">
             Getting Started with GLiCID ⏱️
           </Link>
         </div>
